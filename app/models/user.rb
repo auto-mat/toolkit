ea934ebf27da25424e4c5cd5d5614374b0167940
Petr Dlouhý <petr.dlouhy@email.cz>
2013-10-08 16:46:54 +0200

    fix bug when change of user role is not possible for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :full_name, :display_name, :password, :password_confirmation, :disabled
+  attr_accessible :email, :full_name, :display_name, :password, :password_confirmation, :disabled, :role, :profile_attributes
 
   acts_as_voter
 
