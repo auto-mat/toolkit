e184ff7d659de7e23b8969ed993e628c2e5f13ed
Petr Dlouhý <petr.dlouhy@email.cz>
2018-01-22 09:58:02 +0100

    fix cyclescape.org -> cyklistesobe.cz

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,8 +1,8 @@
 akismet_file = Rails.root.join('config', 'akismet')
 if akismet_file.exist?
   Cyclescape::Application.config.rakismet.key = akismet_file.read.strip
-  Cyclescape::Application.config.rakismet.url = 'http://www.cyclescape.org/'
+  Cyclescape::Application.config.rakismet.url = 'http://www.cyklistesobe.cz/'
 elsif ['development', 'test'].include? Rails.env
   Cyclescape::Application.config.rakismet.key = 'development'
-  Cyclescape::Application.config.rakismet.url = 'http://www.cyclescape.org/'
+  Cyclescape::Application.config.rakismet.url = 'http://www.cyklistesobe.cz/'
 end
